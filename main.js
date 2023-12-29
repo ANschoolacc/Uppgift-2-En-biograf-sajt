49330619c9d1ddb424ff2bb9d220b8dc2127209c
PedalConny <pedalconny@hotmail.com>
2023-12-29 11:02:48 +0100

    added ticket modal

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 async function fetchMovies() {
-  const response = await fetch("https://anschoolacc.github.io/Uppgift-2-En-biograf-sajt/movies.JSON");
+  const response = await fetch(
+    "https://anschoolacc.github.io/Uppgift-2-En-biograf-sajt/movies.JSON"
+  );
   return await response.json();
 }
 
@@ -16,8 +18,7 @@ function renderMovies(container) {
   movies.forEach((movie) => {
     const movieCard = createMovie(movie);
     container.appendChild(movieCard);
-  })
-
+  });
 }
 
 function createMovie(movie) {
@@ -40,3 +41,37 @@ function createMovie(movie) {
   cardCover.style.backgroundImage = `url(${movie.image})`;
   return temp;
 }
+
+const btnsOpenModal = document.querySelectorAll(".movieTemplate__button");
+const btnCloseModal = document.querySelector(".modal__close-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const cancelBtn = document.querySelector(".modal__btns__cancel");
+const btnBook = document.querySelector(".modal__btns__book");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  document.querySelector(".modal__offLine").style.color = "#464646";
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+
+const offlineMessage = function () {
+  console.log("Offline message function is running");
+  document.querySelector(".modal__offLine").style.color = "#800020";
+};
+
+btnBook.addEventListener("click", offlineMessage);
+
+cancelBtn.addEventListener("click", closeModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
